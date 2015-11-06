69a77d15dd12474325dd7b6ddc540e329944f60b
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2015-11-05 23:12:11 -0600

    Initialized a variable

--- Basic4GL/src/com/basic4gl/vm/util/IntHandleResources.java
+++ Basic4GL/src/com/basic4gl/vm/util/IntHandleResources.java
@@ -1,5 +1,6 @@
 package com.basic4gl.vm.util;
 
+import java.util.HashSet;
 import java.util.Set;
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -12,7 +13,7 @@ import java.util.Set;
 
 public abstract class IntHandleResources extends Resources {
 
-	protected Set<Integer> m_handles;
+	protected Set<Integer> m_handles = new HashSet<Integer>();
 
 	protected abstract void DeleteHandle(int handle);
 
