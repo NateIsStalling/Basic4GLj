e39a264147ceb9e4c27ea6a3272a4c71280d35a5
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2015-11-05 22:30:50 -0600

    Added launcher script with command line arguments for LWJGL

---
+++ Basic4GLj/distribution/Basic4GLj.bat
@@ -0,0 +1,3 @@
+::Run Basic4GLj on Windows; requires Java be installed and in your path
+::-Djava.library.path=native/ is needed to show lwjgl window
+java -jar -Djava.library.path=native/ Basic4GLj.jar
