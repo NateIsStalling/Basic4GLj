0ddc45c860b142454bc9e09abedb7bf74e121fb2
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2015-11-05 23:40:59 -0600

    Added interfaces for classes that use OpenGL functions

---
+++ Basic4GL/src/com/basic4gl/lib/util/IB4GLOpenGLText.java
@@ -0,0 +1,31 @@
+package com.basic4gl.lib.util;
+
+/**
+ * Created by Nate on 11/2/2015.
+ */
+public interface IB4GLOpenGLText {
+    // Text mode constants/variables
+    enum TextMode {
+        TEXT_SIMPLE( 0),    // (Default). After any print function, text is automatically rendered as follows:
+        //      * Back buffer is cleared
+        //      * Text is rendered
+        //      * Buffers are swapped
+
+        TEXT_BUFFERED( 1),      // Must explicitly call DrawText(), as before:
+        //      * Back buffer is cleared
+        //      * Text is rendered
+        //      * Buffers are swapped
+
+        TEXT_OVERLAID( 2);       // Same as TEXT_BUFFERED, except DrawText() only renders the text.
+        // Must explictly clear and swap buffers as necessary.
+        private int mMode;
+        TextMode(int mode){
+            mMode = mode;
+        }
+        public int getMode(){ return mMode;}
+    }
+    void Font(int fontTexture);
+    int DefaultFont();
+    void SetTextMode(TextMode mode);
+    void Color(byte red, byte green, byte blue);
+}
