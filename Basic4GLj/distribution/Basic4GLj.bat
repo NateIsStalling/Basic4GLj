342c01216f34e5a1d46c7811ccc76ea7d0f0fcd8
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2015-11-11 18:20:13 -0600

    Reworded comment in launcher script

--- Basic4GLj/distribution/Basic4GLj.bat
+++ Basic4GLj/distribution/Basic4GLj.bat
@@ -1,3 +1,3 @@
 ::Run Basic4GLj on Windows; requires Java be installed and in your path
-::-Djava.library.path=native/ is needed to show lwjgl window
+::-Djava.library.path=native/ is needed for lwjgl library to work properly
 java -jar -Djava.library.path=native/ Basic4GLj.jar
