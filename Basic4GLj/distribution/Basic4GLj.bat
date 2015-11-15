52bee76ed601186966017d1bad09fcb26ea33aa9
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2015-11-15 16:41:25 -0600

    Added logging to launcher script for debugging

--- Basic4GLj/distribution/Basic4GLj.bat
+++ Basic4GLj/distribution/Basic4GLj.bat
@@ -1,3 +1,6 @@
 ::Run Basic4GLj on Windows; requires Java be installed and in your path
 ::-Djava.library.path=native/ is needed for lwjgl library to work properly
-java -jar -Djava.library.path=native/ Basic4GLj.jar
+::Log console output for debugging
+>output.log (
+    java -jar -Djava.library.path=native/ Basic4GLj.jar
+)
