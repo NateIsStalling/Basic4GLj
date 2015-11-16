875a816eb153409fd1e5db870ae7ef7e33bd127e
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2015-11-15 22:14:11 -0600

    Changed BinOperExt and UnOperExt from abstract classes to interfaces

--- Basic4GL/src/com/basic4gl/compiler/util/UnOperExt.java
+++ Basic4GL/src/com/basic4gl/compiler/util/UnOperExt.java
@@ -5,8 +5,8 @@ import com.basic4gl.vm.types.ValType;
 import com.basic4gl.vm.types.OpCode;
 
 //Language extension: Operator overloading
-public abstract class UnOperExt {
-	public abstract boolean run (  Mutable<ValType> regType,     // IN: Current type in register.                                                        OUT: Required type cast before calling function
+public interface UnOperExt {
+	abstract boolean run (  Mutable<ValType> regType,     // IN: Current type in register.                                                        OUT: Required type cast before calling function
             short opCode,          // IN: Operator being applied
             Mutable<Integer> operFunction,      // OUT: Index of VM_CALL_OPERATOR_FUNC function to call
             Mutable<ValType> resultType,  // OUT: Resulting value type
