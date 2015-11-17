2fee263fd4383676a99737ff0379b244b0e58f6c
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2015-11-17 00:35:25 -0600

    Renamed IB4GLFileAccessor to match original source

--- Basic4GL/src/com/basic4gl/lib/util/IB4GLFileAccessor.java
+++ Basic4GL/src/com/basic4gl/lib/util/IB4GLFileAccessor.java
@@ -4,11 +4,11 @@ package com.basic4gl.lib.util;
 //
 /// Used to access files from a plugin.
 /// Plugins can use this object to access files embedded in a standalone exe.
-public interface FileAccessor {
+public interface IB4GLFileAccessor {
     /// Get filename to open for read.
     /// If the file is an embedded file then it will be extracted into a
     /// temporary file whose filename will be returned (in dst parameter).
     /// Otherwise simply returns the filename (in dst parameter).
     /// dst must be at least MAX_PATH characters in length.
-	 public abstract void GetFilenameForRead(String filename, String dst);
+	 void getFilenameForRead(String filename, String dst);
 }
