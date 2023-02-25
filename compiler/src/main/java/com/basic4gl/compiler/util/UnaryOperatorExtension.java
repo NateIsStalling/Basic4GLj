3943ad1c2c2a75fb4e7c8a9694366bfae69c1877
Nathaniel Nielsen <Nate.S.Nielsen@gmail.com>
2023-02-25 18:47:18 -0500

    rename LICENSES folder

---
+++ compiler/src/main/java/com/basic4gl/compiler/util/UnaryOperatorExtension.java
@@ -0,0 +1,24 @@
+package com.basic4gl.compiler.util;
+
+import com.basic4gl.runtime.util.Mutable;
+import com.basic4gl.runtime.types.ValType;
+
+/**
+ * Language extension: Operator overloading
+ */
+public interface UnOperExt {
+	/**
+	 *
+	 * @param regType IN: Current type in register. OUT: Required type cast before calling function
+	 * @param opCode IN: Operator being applied
+	 * @param operFunction OUT: Index of VM_CALL_OPERATOR_FUNC function to call
+	 * @param resultType OUT: Resulting value type
+	 * @param freeTempData OUT: Set to true if temp data needs to be freed
+	 * @return
+	 */
+	boolean run (Mutable<ValType> regType,
+            short opCode,
+            Mutable<Integer> operFunction,
+            Mutable<ValType> resultType,
+            Mutable<Boolean> freeTempData);
+}
