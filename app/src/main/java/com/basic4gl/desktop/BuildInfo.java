1736d3188e73124b81d6992797c885742226176d
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2023-02-26 13:34:20 -0500

    update build info constants

--- app/src/main/java/com/basic4gl/desktop/BuildInfo.java
+++ app/src/main/java/com/basic4gl/desktop/BuildInfo.java
@@ -1,11 +1,11 @@
 package com.basic4gl.desktop;
 
-public class Application {
+public class BuildInfo {
     //Window Constants
     public static final String APPLICATION_NAME = "Basic4GLj";
-    public static final String APPLICATION_VERSION = "Alpha 0.3.0";
-    public static final String APPLICATION_BUILD_DATE = "11/15/2015";
-    public static final String APPLICATION_COPYRIGHT = "(c) 2020, Nathaniel Nielsen";
+    public static final String APPLICATION_VERSION = "Alpha 0.4.0";
+    public static final String APPLICATION_BUILD_DATE = "02/26/2023";
+    public static final String APPLICATION_COPYRIGHT = "(c) 2023, Nathaniel Nielsen";
     public static final String APPLICATION_DESCRIPTION = "Basic4GL for Java";
     public static final String APPLICATION_WEBSITE = "https://github.com/NateIsStalling/Basic4GLj";
     public static final String APPLICATION_CONTACT = "https://github.com/NateIsStalling/Basic4GLj/issues";
