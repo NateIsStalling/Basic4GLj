9693d90f5274bb854f4220a3acb83dff5336ee24
Nathaniel Nielsen <NateIsStalling@users.noreply.github.com>
2026-06-06 20:18:40 -0400

    0.7.0 release - the big compiler and debugger update

--- app/src/main/java/com/basic4gl/desktop/BuildInfo.java
+++ app/src/main/java/com/basic4gl/desktop/BuildInfo.java
@@ -5,7 +5,7 @@ public class BuildInfo {
     public static final String APPLICATION_VERSION_RESOURCE = "applicationVersion";
     public static final String APPLICATION_BUILD_DATE_RESOURCE = "applicationBuildDate";
     public static final String APPLICATION_NAME = "Basic4GLj";
-    public static final String APPLICATION_COPYRIGHT = "(c) 2024, Nathaniel Nielsen";
+    public static final String APPLICATION_COPYRIGHT = "(c) 2026, Nathaniel Nielsen";
     public static final String APPLICATION_DESCRIPTION = "Basic4GL for Java";
     public static final String APPLICATION_WEBSITE = "https://github.com/NateIsStalling/Basic4GLj";
     public static final String APPLICATION_CONTACT = "https://github.com/NateIsStalling/Basic4GLj/issues";
